ce351aa49ffc2316645c6f2741148976e2ea04b3
bhaleraom737@gmail.com <bhaleraom737@gmail.com>
2026-04-19 14:29:25 +0530

    updated backend for deployment

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGO_URI}/whatsappclone`)
+    // await mongoose.connect(`${process.env.MONGO_URI}/attendance_management`)
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error);
